726011f6a8c234b53150599397c47a5d5209f461
406-07-Ranutchai <54184@yupparaj.ac.th>
2026-08-21 10:53:04 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -3,14 +3,12 @@ import streamlit as st
 
 st.title("⏱️ เกมเติมศัพท์จับเวลา")
 
-# 1. กำหนดค่าเริ่มต้นใน session_state ถ้ายังไม่มี
 if "ans1_val" not in st.session_state:
     st.session_state.ans1_val = ""
 if "ans2_val" not in st.session_state:
     st.session_state.ans2_val = ""
 
 
-# 📌 ฟังก์ชันเคลียร์ค่าเมื่อกดปุ่มเริ่มใหม่
 def reset_game():
     st.session_state.ans1_val = ""  # เคลียร์ค่าช่องข้อ 1
     st.session_state.ans2_val = ""  # เคลียร์ค่าช่องข้อ 2
@@ -18,9 +16,6 @@ def reset_game():
     st.session_state.is_ended = False  # ปิด Dialog
 
 
-# --------------------------------------------------
-# 📌 ฟังก์ชัน MessageBox (Dialog)
-# --------------------------------------------------
 @st.dialog("📊 สรุปผลการเล่นเกม")
 def show_result_dialog(ans1, ans2):
     st.balloons()
@@ -29,7 +24,6 @@ def show_result_dialog(ans1, ans2):
     u_ans1 = ans1.strip().lower()
     u_ans2 = ans2.strip().lower()
 
-    # ตรวจข้อ 1
     if u_ans1 == "apple":
         st.success("✅ ข้อ 1: ถูกต้อง")
         score += 1
